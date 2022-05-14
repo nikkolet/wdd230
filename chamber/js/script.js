4f1626bc916049170e21a3393d3ef05afc54d9d2
nikkolet <81433602+nikkolet@users.noreply.github.com>
2022-05-14 00:21:23 -0600

    commit

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -31,3 +31,12 @@ const currentdate = dayName + ", " + todaysdate.getDate() + " " + monthName + ",
 
 document.getElementById('currentdate').textContent = currentdate;
 document.getElementById('curdate').textContent = currentdate;
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+
+x.onclick = toggleMenu;
