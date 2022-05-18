6c39e5e5a01f3bfb8cfe0af7b49ec013fd6d7ed7
nikkolet <81433602+nikkolet@users.noreply.github.com>
2022-05-18 02:22:25 -0600

    commit

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -1,43 +1,39 @@
 window.addEventListener('load', () => {
     const form = document.querySelector("#new-chapter-form");
     const input = document.querySelector("#new-chapter-input");
-    const list_el = document.querySelector("#listcontainer");
+    const list_el = document.querySelector("#chapters");
+
     form.addEventListener('submit', (e) => {
         e.preventDefault();
+
         const chapter = input.value;
 
-        if (!chapter) {
-            alert("Please type in a chapter");
-        } else {
-            console.log("Success!")
-            return;
-        }
-        const chapter_el = document.createElement("div");
-        chapter_el.classList.add("chapter");
+        const chapter_el = document.createElement('div');
+        chapter_el.classList.add('chapter');
 
-        const chapter_content_el = document.createElement("div");
-        chapter_content_el.classList.add("content");
+        const chapter_content_el = document.createElement('div');
+        chapter_content_el.classList.add('content');
 
         chapter_el.appendChild(chapter_content_el);
 
-        const chapter_input_el = document.createElement("input");
-        chapter_input_el.classList.add("text");
-        chapter_input_el.type = "text";
+        const chapter_input_el = document.createElement('input');
+        chapter_input_el.classList.add('text');
+        chapter_input_el.type = 'text';
         chapter_input_el.value = chapter;
-        chapter_input_el.setAttribute("readonly", "readonly");
+        chapter_input_el.setAttribute('readonly', 'readonly');
 
         chapter_content_el.appendChild(chapter_input_el);
 
-        const chapter_actions_el = document.createElement("div");
-        chapter_actions_el.classList.add("actions");
+        const chapter_actions_el = document.createElement('div');
+        chapter_actions_el.classList.add('actions');
 
-        const chapter_edit_el = document.createElement("button");
-        chapter_edit_el.classList.add("edit");
-        chapter_edit_el.innerHTML = "Edit";
+        const chapter_edit_el = document.createElement('button');
+        chapter_edit_el.classList.add('edit');
+        chapter_edit_el.innerText = 'Edit';
 
-        const chapter_delete_el = document.createElement("button");
-        chapter_delete_el.classList.add("delete");
-        chapter_delete_el.innerHTML = "Delete";
+        const chapter_delete_el = document.createElement('button');
+        chapter_delete_el.classList.add('delete');
+        chapter_delete_el.innerText = 'Delete';
 
         chapter_actions_el.appendChild(chapter_edit_el);
         chapter_actions_el.appendChild(chapter_delete_el);
@@ -46,6 +42,21 @@ window.addEventListener('load', () => {
 
         list_el.appendChild(chapter_el);
 
-        input.value = "";
-    })
-})
+        input.value = '';
+
+        chapter_edit_el.addEventListener('click', (e) => {
+            if (chapter_edit_el.innerText.toLowerCase() == "edit") {
+                chapter_edit_el.innerText = "Save";
+                chapter_input_el.removeAttribute("readonly");
+                chapter_input_el.focus();
+            } else {
+                chapter_edit_el.innerText = "Edit";
+                chapter_input_el.setAttribute("readonly", "readonly");
+            }
+        });
+
+        chapter_delete_el.addEventListener('click', (e) => {
+            list_el.removeChild(chapter_el);
+        });
+    });
+});
