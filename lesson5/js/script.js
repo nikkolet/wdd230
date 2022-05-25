0e0c401a9bd1f2b7e7568a4492539f5f6a66dc56
nikkolet <nikkoletashby@Gmail.com>
2022-05-24 20:00:53 -0600

    commit

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -6,57 +6,64 @@ window.addEventListener('load', () => {
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const chapter = input.value;
+        if (input.value === '') {
+            alert('You didn\'t enter text. Please try again.');
+        } else {
 
-        const chapter_el = document.createElement('div');
-        chapter_el.classList.add('chapter');
 
-        const chapter_content_el = document.createElement('div');
-        chapter_content_el.classList.add('content');
+            const chapter = input.value;
 
-        chapter_el.appendChild(chapter_content_el);
+            const chapter_el = document.createElement('div');
+            chapter_el.classList.add('chapter');
 
-        const chapter_input_el = document.createElement('input');
-        chapter_input_el.classList.add('text');
-        chapter_input_el.type = 'text';
-        chapter_input_el.value = chapter;
-        chapter_input_el.setAttribute('readonly', 'readonly');
+            const chapter_content_el = document.createElement('div');
+            chapter_content_el.classList.add('content');
 
-        chapter_content_el.appendChild(chapter_input_el);
+            chapter_el.appendChild(chapter_content_el);
 
-        const chapter_actions_el = document.createElement('div');
-        chapter_actions_el.classList.add('actions');
+            const chapter_input_el = document.createElement('input');
+            chapter_input_el.classList.add('text');
+            chapter_input_el.type = 'text';
+            chapter_input_el.value = chapter;
+            chapter_input_el.setAttribute('readonly', 'readonly');
 
-        const chapter_edit_el = document.createElement('button');
-        chapter_edit_el.classList.add('edit');
-        chapter_edit_el.innerText = 'Edit';
+            chapter_content_el.appendChild(chapter_input_el);
 
-        const chapter_delete_el = document.createElement('button');
-        chapter_delete_el.classList.add('delete');
-        chapter_delete_el.innerText = 'Delete';
+            const chapter_actions_el = document.createElement('div');
+            chapter_actions_el.classList.add('actions');
 
-        chapter_actions_el.appendChild(chapter_edit_el);
-        chapter_actions_el.appendChild(chapter_delete_el);
+            const chapter_edit_el = document.createElement('button');
+            chapter_edit_el.classList.add('edit');
+            chapter_edit_el.innerText = 'Edit';
 
-        chapter_el.appendChild(chapter_actions_el);
+            const chapter_delete_el = document.createElement('button');
+            chapter_delete_el.classList.add('delete');
+            chapter_delete_el.innerText = 'Delete';
 
-        list_el.appendChild(chapter_el);
+            chapter_actions_el.appendChild(chapter_edit_el);
+            chapter_actions_el.appendChild(chapter_delete_el);
 
-        input.value = '';
+            chapter_el.appendChild(chapter_actions_el);
 
-        chapter_edit_el.addEventListener('click', (e) => {
-            if (chapter_edit_el.innerText.toLowerCase() == "edit") {
-                chapter_edit_el.innerText = "Save";
-                chapter_input_el.removeAttribute("readonly");
-                chapter_input_el.focus();
-            } else {
-                chapter_edit_el.innerText = "Edit";
-                chapter_input_el.setAttribute("readonly", "readonly");
-            }
-        });
+            list_el.appendChild(chapter_el);
 
-        chapter_delete_el.addEventListener('click', (e) => {
-            list_el.removeChild(chapter_el);
-        });
+            input.value = '';
+
+            chapter_edit_el.addEventListener('click', (e) => {
+                if (chapter_edit_el.innerText.toLowerCase() == "edit") {
+                    chapter_edit_el.innerText = "Save";
+                    chapter_input_el.removeAttribute("readonly");
+                    chapter_input_el.focus();
+                } else {
+                    chapter_edit_el.innerText = "Edit";
+                    chapter_input_el.setAttribute("readonly", "readonly");
+                }
+            });
+
+            chapter_delete_el.addEventListener('click', (e) => {
+                list_el.removeChild(chapter_el);
+
+            });
+        }
     });
 });
