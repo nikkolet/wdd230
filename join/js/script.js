1e924bc29fa8e87ad5d838a2ae8d472be0a73c05
nikkolet <nikkoletashby@Gmail.com>
2022-06-09 23:17:34 -0600

    commit

--- join/js/script.js
+++ join/js/script.js
@@ -42,3 +42,15 @@ function toggleMenu() {
 const x = document.getElementById('hamburgerBtn');
 
 x.onclick = toggleMenu;
+
+
+var d = new Date();
+
+// Set the value of the "date" field
+document.getElementById("date").value = d.toDateString();
+
+// Set the value of the "time" field
+var hours = d.getHours();
+var mins = d.getMinutes();
+var seconds = d.getSeconds();
+document.getElementById("time").value = hours + ":" + mins + ":" + seconds;
