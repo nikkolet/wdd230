c00aa2ff5a10955d027e8346dca1dc84fdb4b739
nikkolet <nikkoletashby@Gmail.com>
2022-06-09 18:44:30 -0600

    commit

---
+++ join/js/script.js
@@ -0,0 +1,44 @@
+// day names array
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+// long month names array
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+const todaysdate = new Date()
+const dayName = daynames[todaysdate.getDay()];
+const monthName = months[todaysdate.getMonth()];
+const currentdate = dayName + ", " + todaysdate.getDate() + " " + monthName + ", " + todaysdate.getFullYear();
+
+document.getElementById("currentdate").textContent = currentdate;
+document.getElementById("curdate").innerHTML = currentdate;
+
+
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerBtn');
+
+x.onclick = toggleMenu;
