5fccf1006da9e414d3d226ef881a3dcf298aba8f
nikkolet <nikkoletashby@Gmail.com>
2022-06-25 00:47:48 -0600

    commit

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -3,17 +3,18 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=West Valley City&appid=f9588660b59034f500496199128ef328';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=West Valley City&units=imperial&appid=f9588660b59034f500496199128ef328';
 
 apiFetch(url);
 
-async function apiFetch() {
+async function apiFetch(apiURL) {
     try {
-        const response = await fetch(apiFetch);
+        const response = await fetch(apiURL);
         if (response.ok) {
             const data = await response.json();
             console.log(data); // this is for testing the call
             // displayResults(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -25,16 +26,12 @@ async function apiFetch() {
 
 
 function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
-}
-
-function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
 
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = desc;
+    captionDesc.innerHTML = desc;
 }
