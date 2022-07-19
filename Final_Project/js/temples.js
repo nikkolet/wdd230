02b2ffb567f58e805a0d74f363b10d8136b018d4
nikkolet <nikkoletashby@Gmail.com>
2022-07-19 16:51:54 -0600

    commit

--- Final_Project/js/temples.js
+++ Final_Project/js/temples.js
@@ -10,40 +10,40 @@ fetch(requestURL)
     .then(function(jsonObject) {
         console.table(jsonObject); // temporary checking for valid response and data parsing
 
-        const companies = jsonObject['companies'];
+        const temples = jsonObject['temples'];
 
-        companies.forEach(displayCompanies);
+        temples.forEach(displayTemples);
 
 
-        function displayCompanies(company) {
+        function displayTemples(temple) {
             // Create elements to add to the document
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
-            let start = document.createElement("p")
+            let dedicated = document.createElement("p")
             let phone = document.createElement("p")
             let hours = document.createElement("p")
-            let membership = document.createElement("p")
+            let Status = document.createElement("p")
             let portrait = document.createElement('img');
 
             // Change the textContent property of the h2 element to contain the prophet's full name
-            h2.textContent = company.companyname;
-            phone.textContent = company.phone;
-            start.textContent = company.startdate;
-            hours.textContent = company.hours;
-            membership.textContent = "Membership: " + company.membership
+            h2.textContent = temple.name;
+            phone.textContent = "Phone: " + temple.phone;
+            dedicated.textContent = "Dedicated: " + temple.dedicated;
+            hours.textContent = "Hours: " + temple.hours;
+            Status.textContent = "Status: " + temple.status
 
             // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-            portrait.setAttribute('src', company.imageurl);
-            portrait.setAttribute('alt', 'Portait of ' + company.CEO);
+            portrait.setAttribute('src', temple.imageurl);
+            portrait.setAttribute('alt', 'Portait of ' + temple.name);
             portrait.setAttribute('loading', 'lazy');
 
             // Add/append the section(card) with the h2 elemen.t
             card.appendChild(h2);
             card.appendChild(portrait);
-            card.appendChild(start);
+            card.appendChild(dedicated);
             card.appendChild(phone);
             card.appendChild(hours);
-            card.appendChild(membership);
+            card.appendChild(Status);
 
             // Add/append the existing HTML div with the cards class with the section(card)
             document.querySelector('div.cards').appendChild(card);
