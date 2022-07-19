a2fe580fe421fecdb3351472446a90e8efebd2bf
nikkolet <nikkoletashby@Gmail.com>
2022-07-19 14:20:39 -0600

    commit

---
+++ Final_Project/js/temples.js
@@ -0,0 +1,53 @@
+const requestURL = '../Final_Project/json/temples.json';
+const cards = document.querySelector('.cards');
+
+
+
+fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObject) {
+        console.table(jsonObject); // temporary checking for valid response and data parsing
+
+        const companies = jsonObject['companies'];
+
+        companies.forEach(displayCompanies);
+
+
+        function displayCompanies(company) {
+            // Create elements to add to the document
+            let card = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let start = document.createElement("p")
+            let phone = document.createElement("p")
+            let hours = document.createElement("p")
+            let membership = document.createElement("p")
+            let portrait = document.createElement('img');
+
+            // Change the textContent property of the h2 element to contain the prophet's full name
+            h2.textContent = company.companyname;
+            phone.textContent = company.phone;
+            start.textContent = company.startdate;
+            hours.textContent = company.hours;
+            membership.textContent = "Membership: " + company.membership
+
+            // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+            portrait.setAttribute('src', company.imageurl);
+            portrait.setAttribute('alt', 'Portait of ' + company.CEO);
+            portrait.setAttribute('loading', 'lazy');
+
+            // Add/append the section(card) with the h2 elemen.t
+            card.appendChild(h2);
+            card.appendChild(portrait);
+            card.appendChild(start);
+            card.appendChild(phone);
+            card.appendChild(hours);
+            card.appendChild(membership);
+
+            // Add/append the existing HTML div with the cards class with the section(card)
+            document.querySelector('div.cards').appendChild(card);
+
+
+        }
+    });
---
+++ Final_Project/js/weatherapimain.js
@@ -0,0 +1,65 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const humidity = document.querySelector('#humidity');
+const windspeed = document.querySelector('#windspeed');
+const windchill = document.querySelector('windchill');
+
+const currentTemp1 = document.querySelector('#current-temp1');
+const weatherIcon1 = document.querySelector('#weather-icon1');
+const captionDesc1 = document.querySelector('#figcaption1');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=West Valley City&units=imperial&appid=f9588660b59034f500496199128ef328';
+
+apiFetch(url);
+
+async function apiFetch(apiURL) {
+    try {
+        const response = await fetch(apiURL);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // this is for testing the call
+            // displayResults(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// document.querySelector('#button').onclick = calculateWindChill;
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
+    humidity.innerHTML = `<strong>${weatherData.main.humidity.toFixed(1)}</strong>`;
+    windspeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(1)}</strong>`;
+    currentTemp1.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
+
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.innerHTML = desc;
+
+    weatherIcon1.setAttribute('src', iconsrc);
+    weatherIcon1.setAttribute('alt', desc);
+    captionDesc1.innerHTML = desc;
+
+    if ((temp <= 50) && (wSpeed > 3)) {
+
+        var windChill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(wSpeed, 0.16)) + (0.4275 * temp * Math.pow(wSpeed, 0.16));
+
+
+        windChill = Math.round(windChill);
+        document.getElementById("windchill").innerHTML = windChill + "°f";
+    } else {
+        document.getElementById("windchill").innerHTML = "N/A";
+    }
+}
