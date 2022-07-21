7f3e6aa498bdfc1a4b2a37837d048e47b7e649f7
nikkolet <nikkoletashby@Gmail.com>
2022-07-20 20:32:13 -0600

    commit

--- Final_Project/js/script.js
+++ Final_Project/js/script.js
@@ -9,10 +9,13 @@ let day = d.getDate();
 
 const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let dayofweek = weekday[d.getDay()];
-let today = Date.now;
+let tomorrow = weekday[d.getDay() + 1];
+let dayafter = weekday[d.getDay() + 2];
 const year = d.getFullYear();
 
-document.querySelector("#today").textContent = today;
+document.querySelector("#today").textContent = dayofweek;
+document.querySelector("#tomorrow").textContent = tomorrow;
+document.querySelector("#dayafter").textContent = dayafter;
 
 // document.querySelector(".date").textContent = dayofweek + ", " + day + "  " + newmonth + " " + year;
 document.querySelector("#lastmodified").textContent = document.lastModified;
